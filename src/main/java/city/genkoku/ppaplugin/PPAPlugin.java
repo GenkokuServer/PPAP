d24382c020cc9e7d94a379f8f9f16f0945a99f06
nashi2603 <nashi.20306.y@gmail.com>
2020-09-11 20:54:16 +0900

    F--- u POOP Player

---
+++ src/main/java/city/genkoku/ppaplugin/PPAPlugin.java
@@ -0,0 +1,16 @@
+package city.genkoku.ppaplugin;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class PPAPlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
